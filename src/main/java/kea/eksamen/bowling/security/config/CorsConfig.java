81c1046536e9e01813f677d468ae594f403cbcbf
TheDanishMexican <danieljchristjansen@gmail.com>
2024-05-06 18:49:26 +0200

    added cors config for frontend communication

--- src/main/java/kea/eksamen/bowling/security/config/CorsConfig.java
+++ src/main/java/kea/eksamen/bowling/security/config/CorsConfig.java
@@ -1,10 +1,13 @@
-package dat3.security.config;
+package kea.eksamen.bowling.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.util.Arrays;
 
 @Configuration
@@ -14,19 +17,19 @@ public class CorsConfig {
     private static final String[] ALLOWED_ORIGINS = new String[] {
             "*",
             "https://example.com/",
-            "http://127.0.0.1:5500"
+            "http://127.0.0.1:5500",
+            "http://localhost:5173"
     };
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        //Use the line below instead, if you want to allow specific headers only
-        //configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "X-Requested-With"));
-        configuration.setAllowedOriginPatterns(Arrays.asList(ALLOWED_ORIGINS));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(ALLOWED_ORIGINS)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
