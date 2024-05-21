df061bcc979dd0f2f31417a265e86dc4eaec64e9
fili0727 <123998888+fili0727@users.noreply.github.com>
2024-05-21 15:02:30 +0200

    deleted security

--- src/main/java/kea/eksamen/bowling/security/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-//package dat3.security.repository;
-//
-//import dat3.security.entity.Role;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RoleRepository extends JpaRepository<Role, String> {
-//}
